b7da22a7ab4db2f5daec4db916997a02dc2c0193
James <jamesmilner22@hotmail.com>
2023-07-25 21:19:57 +0100

    fix: ensure that coordinate precision is limited when dragging a feature

--- src/modes/select/behaviors/drag-feature.behavior.ts
+++ src/modes/select/behaviors/drag-feature.behavior.ts
@@ -4,6 +4,7 @@ import { FeaturesAtMouseEventBehavior } from "./features-at-mouse-event.behavior
 import { Position } from "geojson";
 import { SelectionPointBehavior } from "./selection-point.behavior";
 import { MidPointBehavior } from "./midpoint.behavior";
+import { limitPrecision } from "../../../geometry/limit-decimal-precision";
 
 export class DragFeatureBehavior extends TerraDrawModeBehavior {
 	constructor(
@@ -100,8 +101,16 @@ export class DragFeatureBehavior extends TerraDrawModeBehavior {
 					this.dragPosition[1] - mouseCoord[1],
 				];
 
-				const updatedLng = coordinate[0] - delta[0];
-				const updatedLat = coordinate[1] - delta[1];
+				// Keep precision limited when calculating new coordinates
+				const updatedLng = limitPrecision(
+					coordinate[0] - delta[0],
+					this.config.coordinatePrecision
+				);
+
+				const updatedLat = limitPrecision(
+					coordinate[1] - delta[1],
+					this.config.coordinatePrecision
+				);
 
 				// Ensure that coordinates are valid
 				if (
@@ -113,7 +122,7 @@ export class DragFeatureBehavior extends TerraDrawModeBehavior {
 					return false;
 				}
 
-				updatedCoords[i] = [updatedLng, coordinate[1] - delta[1]];
+				updatedCoords[i] = [updatedLng, updatedLat];
 			}
 
 			// Set final coordinate identical to first

a7ad32acf530080359a1ff93b96c9d3e81f6a9d1
James Milner <jamesmilner22@hotmail.com>
2023-10-28 18:39:44 +0100

    chore: fix nocheck testing on windows

--- jest.config.ts
+++ jest.config.ts
@@ -1,27 +1,15 @@
-let options = {};
+console.log("===== Using ts-jest ======");
 
-if (process.env.NO_CHECK) {
-	options = {
-		transform: {
-			"^.+\\.(t|j)sx?$": "@swc/jest",
+module.exports = {
+	preset: "ts-jest",
+	testEnvironment: "node",
+	coveragePathIgnorePatterns: ["<rootDir>/src/test/"],
+	setupFilesAfterEnv: ["<rootDir>/src/test/jest.matchers.ts"],
+	collectCoverage: true,
+	collectCoverageFrom: ["./src/**"],
+	coverageThreshold: {
+		global: {
+			lines: 65,
 		},
-		coveragePathIgnorePatterns: ["<rootDir>/src/test/"],
-		setupFilesAfterEnv: ["<rootDir>/src/test/jest.matchers.ts"],
-	};
-} else {
-	options = {
-		preset: "ts-jest",
-		testEnvironment: "node",
-		coveragePathIgnorePatterns: ["<rootDir>/src/test/"],
-		setupFilesAfterEnv: ["<rootDir>/src/test/jest.matchers.ts"],
-		collectCoverage: true,
-		collectCoverageFrom: ["./src/**"],
-		coverageThreshold: {
-			global: {
-				lines: 65,
-			},
-		},
-	};
-}
-
-module.exports = options;
+	},
+};

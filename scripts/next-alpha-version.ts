28f3102dc2881f5070a5c88b62b86b0a43c7f2c1
James Milner <jamesmilner22@hotmail.com>
2024-07-28 19:58:37 +0100

    chore: add script for creating beta release

--- scripts/next-alpha-version.ts
+++ scripts/next-alpha-version.ts
@@ -2,7 +2,10 @@ import { readFileSync } from "fs";
 
 const packageJson = JSON.parse(readFileSync("./package.json", "utf-8"));
 
+// If we're still in alpha, we want to start beta from version 1
 if (packageJson.version.includes("alpha")) {
+	console.log(1);
+} else if (packageJson.version.includes("beta")) {
 	const currentVersion = packageJson.version.split(".").pop();
 
 	if (
@@ -17,5 +20,5 @@ if (packageJson.version.includes("alpha")) {
 
 	console.log(nextVersion);
 } else {
-	throw new Error("This script is for alpha releases only");
+	throw new Error("This script is for beta releases only");
 }

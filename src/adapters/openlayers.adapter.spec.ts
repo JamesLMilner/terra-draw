ae30ff84270b209873dabb3cf1439b9d34773739
AnneFiger <85521048+AnneFiger@users.noreply.github.com>
2024-07-30 16:40:12 +0100

    test: add extra unit test to openlayers.adaptor.spec (#300)

--- src/adapters/openlayers.adapter.spec.ts
+++ src/adapters/openlayers.adapter.spec.ts
@@ -1,22 +1,33 @@
 import { TerraDrawOpenLayersAdapter } from "./openlayers.adapter";
+import Map from "ol/Map";
+import { getMockPointerEvent } from "../test/mock-pointer-event";
 
 jest.mock("ol/style/Circle", () => jest.fn());
 jest.mock("ol/style/Fill", () => jest.fn());
 jest.mock("ol/style/Stroke", () => jest.fn());
 jest.mock("ol/style/Style", () => jest.fn());
-jest.mock("ol/proj", () => jest.fn());
+jest.mock("ol/proj", () => ({ toLonLat: () => [45, 45] }));
 jest.mock("ol/geom/Geometry", () => jest.fn());
+
+const createMockOLMap = () => {
+	return {
+		getViewport: jest.fn(() => ({
+			setAttribute: jest.fn(),
+			querySelectorAll: jest.fn(() => [
+				{
+					getBoundingClientRect: jest.fn(() => ({ top: 0, left: 0 })),
+				},
+			]),
+		})),
+		addLayer: jest.fn(),
+	} as unknown as Map;
+};
 
 describe("TerraDrawOpenLayersAdapter", () => {
 	describe("constructor", () => {
 		it("instantiates the adapter correctly", () => {
 			const adapter = new TerraDrawOpenLayersAdapter({
-				map: {
-					getViewport: jest.fn(() => ({
-						setAttribute: jest.fn(),
-					})),
-					addLayer: jest.fn(),
-				} as any,
+				map: createMockOLMap(),
 				lib: {
 					GeoJSON: jest.fn(),
 					VectorSource: jest.fn(),
@@ -36,6 +47,28 @@ describe("TerraDrawOpenLayersAdapter", () => {
 			expect(adapter.project).toBeDefined();
 			expect(adapter.unproject).toBeDefined();
 			expect(adapter.setCursor).toBeDefined();
+		});
+	});
+
+	describe("getLngLatFromEvent", () => {
+		let adapter: TerraDrawOpenLayersAdapter;
+		const map = createMockOLMap() as Map;
+		beforeEach(() => {
+			adapter = new TerraDrawOpenLayersAdapter({
+				map,
+				lib: {
+					GeoJSON: jest.fn(),
+					VectorSource: jest.fn(),
+					VectorLayer: jest.fn(),
+				} as any,
+			});
+		});
+		it("getLngLatFromEvent returns correct coordinates", () => {
+			// Mock the getCoordinateFromPixel function
+			map.getCoordinateFromPixel = jest.fn(() => [0, 0]);
+
+			const result = adapter.getLngLatFromEvent(getMockPointerEvent());
+			expect(result).toEqual({ lat: 45, lng: 45 });
 		});
 	});
 });

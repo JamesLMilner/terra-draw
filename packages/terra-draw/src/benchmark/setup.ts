07aafb90cf5289e7a0979b83ef7573e4583b7ede
James Milner <jamesmilner22@hotmail.com>
2025-10-04 20:10:48 +0100

    chore(terra-draw): add benchmarking for the public Terra Draw API

---
+++ packages/terra-draw/src/benchmark/setup.ts
@@ -0,0 +1,138 @@
+import { FeatureId } from "../extend";
+import {
+	GeoJSONStoreFeatures,
+	TerraDraw,
+	TerraDrawPointMode,
+	TerraDrawPolygonMode,
+	TerraDrawSelectMode,
+} from "../terra-draw";
+import { TerraDrawTestAdapter } from "../test/test-adapter";
+import { uuid4 } from "../util/id";
+import { Bench } from "tinybench";
+
+export const createPointFeatures = (n: number) =>
+	new Array(n).fill({}).map((_, i) => ({
+		id: uuid4(),
+		type: "Feature",
+		geometry: {
+			type: "Point",
+			coordinates: [Math.min(i, 180), Math.min(i, 90)],
+		},
+		properties: {
+			mode: "point",
+		},
+	})) as GeoJSONStoreFeatures[];
+
+export const createDraw = () => {
+	const draw = new TerraDraw({
+		adapter: new TerraDrawTestAdapter({ lib: {} }),
+		modes: [
+			new TerraDrawPointMode(),
+			new TerraDrawPolygonMode(),
+			new TerraDrawSelectMode(),
+		],
+	});
+
+	return draw;
+};
+
+export type BenchmarkProps = {
+	draw: TerraDraw;
+	features: GeoJSONStoreFeatures[];
+	featureIds: FeatureId[];
+	randomFeatureId: FeatureId;
+};
+
+export type BenchmarkTask = {
+	name: string;
+	beforeEach: (benchmarkProps: BenchmarkProps) => void;
+	task: (benchmarkProps: BenchmarkProps) => void;
+	featureCount?: number;
+	loopCount?: number;
+	enabled?: boolean;
+};
+
+export const createBenchmark = (
+	benmarkName: string,
+	tasks: BenchmarkTask[],
+) => {
+	const bench = new Bench({ name: benmarkName });
+
+	tasks.forEach(
+		({ name, beforeEach, task, enabled, featureCount, loopCount }) => {
+			const features = createPointFeatures(featureCount || 100);
+			const featureIds = features.map((feature) => feature.id as FeatureId);
+
+			const benchmarkProps: BenchmarkProps = {
+				draw: createDraw(),
+				features,
+				featureIds,
+				randomFeatureId:
+					featureIds[Math.floor(Math.random() * featureIds.length)],
+			};
+
+			const isEnabled = enabled === false ? false : true;
+
+			const finalTask =
+				loopCount && loopCount > 1
+					? (props: BenchmarkProps) => {
+							for (let i = 0; i < loopCount; i++) {
+								task(props);
+							}
+						}
+					: task;
+
+			isEnabled &&
+				bench.add(
+					loopCount ? `${name} (x${loopCount})` : name,
+					() => {
+						finalTask(benchmarkProps);
+					},
+					{
+						beforeEach: () => {
+							benchmarkProps.randomFeatureId =
+								featureIds[Math.floor(Math.random() * featureIds.length)];
+							beforeEach(benchmarkProps);
+						},
+					},
+				);
+		},
+	);
+
+	return bench;
+};
+
+export const processBenchmarks = (
+	bench: Bench,
+	benchmarkTasks: BenchmarkTask[],
+) => {
+	const results = bench.results.map((result, i) => {
+		return {
+			name: benchmarkTasks[i].name,
+			opsPerSecond: parseInt(
+				(benchmarkTasks[i].loopCount
+					? result!.throughput!.mean * benchmarkTasks[i].loopCount
+					: result!.throughput!.mean
+				).toFixed(0),
+			),
+			averageTimeMs: parseFloat(
+				(benchmarkTasks[i].loopCount
+					? result!.latency!.mean / benchmarkTasks[i].loopCount
+					: result!.latency!.mean
+				).toFixed(8),
+			),
+		};
+	});
+
+	return results;
+};
+
+export const logBenchmarkResults = (
+	results: { name: string; opsPerSecond: number; averageTimeMs: number }[],
+) => {
+	console.table(
+		results.sort(
+			(resultOne, resultTwo) => resultOne.opsPerSecond - resultTwo.opsPerSecond,
+		),
+	);
+};
---
+++ packages/terra-draw/src/terra-draw.benchmark.ts
@@ -0,0 +1,285 @@
+import {
+	TerraDraw,
+	TerraDrawMouseEvent,
+	TerraDrawPolygonMode,
+} from "./terra-draw";
+
+import {
+	BenchmarkTask,
+	createBenchmark,
+	createDraw,
+	logBenchmarkResults,
+	processBenchmarks,
+} from "./benchmark/setup";
+
+(async () => {
+	const DrawAPI: TerraDraw = createDraw();
+
+	const benchmarkName = "Terra Draw API";
+
+	const featureCount = 100;
+
+	const benchmarkTasks: BenchmarkTask[] = [
+		{
+			name: DrawAPI.getMode.name,
+			beforeEach: (benchmarkProps) => {
+				benchmarkProps.draw = createDraw();
+				benchmarkProps.draw.start();
+			},
+			task: ({ draw }) => {
+				draw.getMode();
+			},
+			loopCount: 1000000,
+		},
+		{
+			name: DrawAPI.getModeState.name,
+			beforeEach: (benchmarkProps) => {
+				benchmarkProps.draw = createDraw();
+				benchmarkProps.draw.start();
+			},
+			task: ({ draw }) => {
+				draw.getModeState();
+			},
+			loopCount: 1000000,
+		},
+		{
+			name: DrawAPI.addFeatures.name,
+			beforeEach: (benchmarkProps) => {
+				benchmarkProps.draw = createDraw();
+				benchmarkProps.draw.start();
+			},
+			task: ({ draw, features }) => {
+				draw.addFeatures([features[0]]);
+			},
+		},
+		{
+			name: DrawAPI.removeFeatures.name,
+			beforeEach: (benchmarkProps) => {
+				benchmarkProps.draw = createDraw();
+				benchmarkProps.draw.start();
+				benchmarkProps.draw.addFeatures(benchmarkProps.features);
+			},
+			task: ({ draw, randomFeatureId }) => {
+				draw.removeFeatures([randomFeatureId]);
+			},
+		},
+		{
+			name: DrawAPI.hasFeature.name,
+			beforeEach: (benchmarkProps) => {
+				benchmarkProps.draw = createDraw();
+				benchmarkProps.draw.start();
+				benchmarkProps.draw.addFeatures(benchmarkProps.features);
+			},
+			task: ({ draw, randomFeatureId }) => {
+				draw.hasFeature(randomFeatureId);
+			},
+			loopCount: 1000,
+			enabled: true,
+		},
+		{
+			name: DrawAPI.getFeatureId.name,
+			beforeEach: (benchmarkProps) => {
+				benchmarkProps.draw = createDraw();
+				benchmarkProps.draw.start();
+				benchmarkProps.draw.addFeatures(benchmarkProps.features);
+			},
+			task: ({ draw }) => {
+				draw.getFeatureId();
+			},
+			loopCount: 1000,
+			enabled: true,
+		},
+		{
+			name: DrawAPI.selectFeature.name,
+			beforeEach: (benchmarkProps) => {
+				benchmarkProps.draw = createDraw();
+				benchmarkProps.draw.start();
+				benchmarkProps.draw.addFeatures(benchmarkProps.features.slice(0, 1));
+			},
+			task: ({ draw, featureIds }) => {
+				draw.selectFeature(featureIds[0]);
+			},
+			loopCount: 1000,
+			enabled: true,
+		},
+		{
+			name: DrawAPI.deselectFeature.name,
+			beforeEach: (benchmarkProps) => {
+				benchmarkProps.draw = createDraw();
+				benchmarkProps.draw.start();
+				benchmarkProps.draw.addFeatures(benchmarkProps.features.slice(0, 1));
+				benchmarkProps.draw.selectFeature(benchmarkProps.featureIds[0]);
+			},
+			task: ({ draw, featureIds }) => {
+				draw.deselectFeature(featureIds[0]);
+			},
+			loopCount: 1000,
+			enabled: true,
+		},
+		{
+			name: DrawAPI.clear.name,
+			beforeEach: (benchmarkProps) => {
+				benchmarkProps.draw = createDraw();
+				benchmarkProps.draw.start();
+				benchmarkProps.draw.addFeatures(benchmarkProps.features);
+			},
+			task: ({ draw }) => {
+				draw.clear();
+			},
+			loopCount: 1000,
+			enabled: true,
+		},
+		{
+			name: DrawAPI.getSnapshot.name,
+			beforeEach: (benchmarkProps) => {
+				benchmarkProps.draw = createDraw();
+				benchmarkProps.draw.start();
+				benchmarkProps.draw.addFeatures(benchmarkProps.features);
+			},
+			task: ({ draw }) => {
+				draw.getSnapshot();
+			},
+			loopCount: 100,
+			enabled: true,
+		},
+		{
+			name: DrawAPI.getSnapshotFeature.name,
+			beforeEach: (benchmarkProps) => {
+				benchmarkProps.draw = createDraw();
+				benchmarkProps.draw.start();
+				benchmarkProps.draw.addFeatures(benchmarkProps.features);
+			},
+			task: ({ draw, randomFeatureId }) => {
+				draw.getSnapshotFeature(randomFeatureId);
+			},
+			loopCount: 1000,
+			enabled: true,
+		},
+		{
+			name: DrawAPI.updateModeOptions.name,
+			beforeEach: (benchmarkProps) => {
+				benchmarkProps.draw = createDraw();
+				benchmarkProps.draw.start();
+				benchmarkProps.draw.addFeatures(benchmarkProps.features);
+			},
+			task: ({ draw }) => {
+				draw.updateModeOptions<typeof TerraDrawPolygonMode>("polygon", {
+					snapping: {
+						toCoordinate: true,
+						toLine: true,
+					},
+					pointerDistance: 40,
+					editable: true,
+					showCoordinatePoints: true,
+				});
+			},
+			enabled: true,
+			loopCount: 100,
+		},
+		{
+			name: DrawAPI.updateFeatureProperties.name,
+			beforeEach: (benchmarkProps) => {
+				benchmarkProps.draw = createDraw();
+				benchmarkProps.draw.start();
+				benchmarkProps.draw.addFeatures(benchmarkProps.features);
+			},
+			task: ({ draw, randomFeatureId }) => {
+				draw.updateFeatureProperties(randomFeatureId, {
+					test: "property",
+					anotherProperty: 123,
+				});
+			},
+			enabled: true,
+			loopCount: 100,
+		},
+		{
+			name: DrawAPI.updateFeatureGeometry.name,
+			beforeEach: (benchmarkProps) => {
+				benchmarkProps.draw = createDraw();
+				benchmarkProps.draw.start();
+				benchmarkProps.draw.addFeatures(benchmarkProps.features);
+			},
+			task: ({ draw, randomFeatureId }) => {
+				draw.updateFeatureGeometry(randomFeatureId, {
+					type: "Point",
+					coordinates: [0, 0],
+				});
+			},
+			enabled: true,
+			loopCount: 100,
+		},
+		{
+			name: DrawAPI.getFeaturesAtLngLat.name,
+			beforeEach: (benchmarkProps) => {
+				benchmarkProps.draw = createDraw();
+				benchmarkProps.draw.start();
+				benchmarkProps.draw.addFeatures(benchmarkProps.features);
+			},
+			task: ({ draw }) => {
+				draw.getFeaturesAtLngLat({ lng: 0, lat: 0 });
+			},
+			enabled: true,
+			loopCount: 100,
+		},
+		{
+			name: DrawAPI.getFeaturesAtPointerEvent.name,
+			beforeEach: (benchmarkProps) => {
+				benchmarkProps.draw = createDraw();
+				benchmarkProps.draw.start();
+				benchmarkProps.draw.addFeatures(benchmarkProps.features);
+			},
+			task: ({ draw }) => {
+				draw.getFeaturesAtPointerEvent({
+					clientX: 0,
+					clientY: 0,
+					buttons: 0,
+				} as PointerEvent);
+			},
+			enabled: true,
+			loopCount: 100,
+		},
+		{
+			name: DrawAPI.on.name,
+			beforeEach: (benchmarkProps) => {
+				benchmarkProps.draw = createDraw();
+				benchmarkProps.draw.start();
+			},
+			task: ({ draw }) => {
+				draw.on("change", () => {});
+			},
+			enabled: true,
+			loopCount: 10000,
+		},
+
+		// These take too long to run if enabled
+		{
+			name: DrawAPI.start.name,
+			beforeEach: (benchmarkProps) => {
+				benchmarkProps.draw = createDraw();
+			},
+			task: ({ draw }) => {
+				draw.start();
+			},
+			enabled: false,
+		},
+		{
+			name: DrawAPI.stop.name,
+			beforeEach: (benchmarkProps) => {
+				benchmarkProps.draw = createDraw();
+				benchmarkProps.draw.start();
+			},
+			task: ({ draw }) => {
+				draw.stop();
+			},
+			enabled: false,
+		},
+	];
+
+	const bench = createBenchmark(benchmarkName, benchmarkTasks);
+
+	await bench.runSync();
+
+	const results = processBenchmarks(bench, benchmarkTasks);
+
+	logBenchmarkResults(results);
+})();

327e0c8d3a05d10fba9905f19461e4360538ab0e
James Milner <jamesmilner22@hotmail.com>
2026-04-15 20:29:42 +0100

    chore(terra-draw): ensure package-lock.json files are updated on release (#864)

--- release.js
+++ release.js
@@ -21,6 +21,13 @@ module.exports = (packageName, packageJsonPath, changelogPath) => ({
 			return commit;
 		},
 	},
+	scripts: {
+		// After we bump but before we commit, we want to update all the other packages that have a dependency on the package we just bumped
+		// to make sure they are using the correct version. We also want to update the package-lock.json file to make sure it is in sync with
+		// the updated package.json files.
+		postbump:
+			"cd ../.. && node update.mjs && npm install --package-lock-only --no-audit --no-fund && git add packages/*/package.json package-lock.json",
+	},
 	changelogFile: changelogPath,
 	releaseCommitMessageFormat: `chore(${packageName}): release version {{currentTag}}`,
 });

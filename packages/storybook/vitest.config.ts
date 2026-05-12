881b437eba7309876d95b3a0fda387e5dbcdbe39
James Milner <jamesmilner22@hotmail.com>
2026-05-12 19:08:28 +0100

    chore(terra-draw): remove CJS warning for npm run unused

---
+++ packages/storybook/vitest.config.ts
@@ -0,0 +1,31 @@
+import { defineConfig } from "vitest/config";
+
+import { storybookTest } from "@storybook/addon-vitest/vitest-plugin";
+
+import { playwright } from "@vitest/browser-playwright";
+
+// More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
+export default defineConfig({
+	test: {
+		projects: [
+			{
+				extends: true,
+				plugins: [
+					// The plugin will run tests for the stories defined in your Storybook config
+					// See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
+					storybookTest({ configDir: ".storybook" }),
+				],
+				test: {
+					name: "storybook",
+					browser: {
+						enabled: true,
+						headless: true,
+						provider: playwright({}),
+						instances: [{ browser: "chromium" }],
+					},
+					setupFiles: [".storybook/vitest.setup.ts"],
+				},
+			},
+		],
+	},
+});

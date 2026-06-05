c3224641eff1b019ef366a481850007407199fb4
James Milner <jamesmilner22@hotmail.com>
2026-06-05 22:34:07 +0100

    fix(terra-draw): extend the list of possible cursor types

--- packages/terra-draw/src/common.ts
+++ packages/terra-draw/src/common.ts
@@ -68,13 +68,42 @@ export type Cursor = Parameters<SetCursor>[0];
 
 export type SetCursor = (
 	cursor:
-		| "unset"
+		| "alias"
+		| "all-scroll"
+		| "auto"
+		| "cell"
+		| "col-resize"
+		| "context-menu"
+		| "copy"
+		| "crosshair"
+		| "default"
+		| "e-resize"
+		| "ew-resize"
 		| "grab"
 		| "grabbing"
-		| "crosshair"
+		| "help"
+		| "move"
+		| "n-resize"
+		| "ne-resize"
+		| "nesw-resize"
+		| "no-drop"
+		| "none"
+		| "not-allowed"
+		| "ns-resize"
+		| "nw-resize"
+		| "nwse-resize"
 		| "pointer"
+		| "progress"
+		| "row-resize"
+		| "s-resize"
+		| "se-resize"
+		| "sw-resize"
+		| "text"
+		| "vertical-text"
+		| "w-resize"
 		| "wait"
-		| "move",
+		| "zoom-in"
+		| "zoom-out",
 ) => void;
 
 export type Project = (lng: number, lat: number) => CartesianPoint;
